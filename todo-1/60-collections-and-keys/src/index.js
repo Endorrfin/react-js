b7380183728e63fda83c598dca14eb94582faa39
Vasyl Krupka <krupka.ua@gmail.com>
2019-12-30 08:01:40 +0200

    60-collections-and-keys

---
+++ todo-1/60-collections-and-keys/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+
+  
+
+
+const App = () => {
+
+
+  const todoData = [
+    { label: 'Drink coffee', important: false, id: 1},
+    { label: 'Make Awesome App', important: true, id: 2},
+    { label: 'Have a lunch', important: false, id: 3},
+  ]
+
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={todoData}/>
+    </div>
+  );
+};
+ 
+  
+ReactDom.render(<App />,
+  document.getElementById('root'));
