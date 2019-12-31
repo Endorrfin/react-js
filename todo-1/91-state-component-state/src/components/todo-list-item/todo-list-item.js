96d7c33815e2e2293d9fb19d2c3774e389adb9a8
Vasyl Krupka <krupka.ua@gmail.com>
2019-12-31 02:15:58 +0200

    91-state-component-state

---
+++ todo-1/91-state-component-state/src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+import './todo-list-item.css';
+
+export default class TodoListItem extends Component {
+
+
+    state = {
+      done: false,
+      important: false
+    };
+
+
+  // *ПОДХОД 2 - ЧТОБ НЕ ПОТЕРЯТЬ THIS создаем конструктор
+  // constructor() {
+  //   super();
+
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${this.props.label}`)
+  //   }
+  // }
+
+  // *ПОДХОД 3 - ЧТОБ НЕ ПОТЕРЯТЬ THIS используем новый синтаксис class field
+    onLabelClick = () => {
+      this.setState(({done}) => {
+        return {
+          done: !done
+        };
+      });
+    };
+
+    onMarkImportant = () => {
+      this.setState(({important}) => {
+        return {
+          important: !important
+        };
+      });
+    };
+
+
+ 
+
+  render() {
+
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = "todo-list-item";
+    if(done) {
+      classNames += ' done';
+    }
+
+    if(important) {
+      classNames += ' important';
+    }
+
+  
+    return (
+      <span className={classNames}>
+        <span
+          className="todo-list-item-label"
+          onClick={ this.onLabelClick } >
+          {label}
+        </span>
+          
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}>
+          <i className="fa fa-exclamation" />
+        </button>
+  
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleted}>
+          <i className="fa fa-trash-o" />
+        </button>
+  
+      </span>
+    );
+  };
+}
