7eaa19d1cb33529e8d082391e4994960a8a564d5
Vasyl Krupka <krupka.ua@gmail.com>
2019-12-31 16:34:44 +0200

    40-props

---
+++ todo-2/40-props/src/components/todo-list.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import TodoListItem from './todo-list-item';
+
+const TodoList = () => {
+  return (
+
+    <ul>
+      <li><TodoListItem label="Drink Coffee" /></li>
+      <li><TodoListItem 
+        label="Build React app"
+        important /></li>
+    </ul>
+  )
+}
+
+export default TodoList;
