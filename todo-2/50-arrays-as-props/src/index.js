a3558840f1cddc208394f68c9680450fa2bf0ec4
Vasyl Krupka <krupka.ua@gmail.com>
2019-12-31 16:35:59 +0200

    50-arrays-as-props

---
+++ todo-2/50-arrays-as-props/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppHeader from './components/app-header';
+import TodoList from './components/todo-list';
+import SearchPanel from './components/search-panel';
+
+
+const App = () => {
+
+  const tododata = [
+    {label: "Drink coffee", important: false},
+    {label: "Make Awesome App", important: true},
+    {label: "Have a lunch", important: false}
+  ];
+
+
+  return (
+    <div>
+    <AppHeader />
+    <SearchPanel />
+    <TodoList todos={tododata} />
+  </div>
+  );
+};
+
+
+
+
+ReactDOM.render(<App/>,
+   document.getElementById('root'));
