a9f051e5cd26465f9ef2f5d4ee7ae390c5628cfb
Vasyl Krupka <krupka.ua@gmail.com>
2019-12-31 12:16:43 +0200

    100-adding-removing-items

---
+++ todo-1/100-adding-removing-items/src/components/todo-list/todo-list.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import TodoListItem from '../todo-list-item/todo-list-item';
+import './todo-list.css';
+
+
+const TodoList = ( { todos, onDeleted }) => {
+
+  const elements = todos.map((item) => {
+
+    // применяем 2 параметра: id = 1-й параметр, ...itemProps - 2-й параметр
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={id} className="list-group-item">
+      {/* Можно передвать все свойства объекта в компонент используя Object Spread оператор (не перечисляя каждое) */}
+        <TodoListItem 
+          { ...itemProps }
+          onDeleted={() => onDeleted(id)} />
+      </li>
+    );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
+
+
+export default TodoList;
